4f5daf2305507606055241c55e9f3778bce6e047
Joseph Kesselring <josephkesselring@gmail.com>
2012-04-03 18:09:20 -0400

    Added comments and todo.

--- py301.py
+++ py301.py
@@ -1,22 +1,38 @@
+#Currently this does not check to make sure the data being processed is in fact a valid url.
+#Good practices dictate this should be updated to check for and handle this process.
 import csv
 from Tkinter import Tk
 from tkFileDialog import askopenfilename, asksaveasfile
+#hide tk gui.
 Tk().withdraw()
+#prompt user for .csv to work with.
 fileToRead = askopenfilename(defaultextension="*.csv", title="Select .CSV")
+#Print basic author details.
 copy = "CSV 301 Redirect\nDeveloped by: Joseph Kesselring (CodeMob, LLC)\nMarch 2012\n\n"
 print copy
+#these fields are from Google's crawl_errors *.csv file.
 fields = ['URL', 'Response Code']
 read = csv.DictReader(open(fileToRead, 'rb'), fields)
+#prompt user for location and file to save the results to. 
 fh = asksaveasfile()
-http = raw_input("Please input domain name of site: (http:// and trailing / will be added automatically)\n").strip()
+#get domain name for redirection.
+http = raw_input("Please input domain name of site: (http://, https://, and/or trailing '/' will be added automatically)\n").strip()
+#make sure http is a website.
+
+#add prefix and suffix.
 if(http[:6] != "http://"):
     http = "http://" + http
+elif(http[:7] != "https://"):
+    http = "https://" + http
 if(http[-1] != "/"):
     http = http + "/"
 offset = http.__len__()
 url = ""
-for row in read: 
+#write lines to file.
+for row in read:
     if(row['Response Code'] == "404"):
         line = "redirect 301 " + row['URL'][offset:] + " " + http + "\n"
+        #this works, but should this be fh.write(line) instead?
         fh.writelines(line)
+#Let user know everything is done.
 print "\nAll Done!"
